601d75fe8b4abdb69d428785b9e94d8fb5d91db7
VladOs <cherednichenkov2000@gmail.com>
2022-07-24 10:14:00 +0300

    Вивід даних шаблоном

---
+++ dzju/teka/forms.py
@@ -0,0 +1,27 @@
+from .models import Kniteka
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+class KnitekaForm(ModelForm):
+    class Meta:
+        model = Kniteka
+        fields = ['title', 'anons', 'texstura', 'date']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Назва'
+            }),
+            'anons': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс'
+            }),
+            'texstura': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст статті'
+            }),
+            'date': DateTimeInput(attrs={
+                'class': 'form-control datetimepicker-input',
+                'placeholder': 'YYYY-mm-dd H:M:S'
+            }),
+
+        }
--- dzju/teka/urls.py
+++ dzju/teka/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.teka_home, name='teka_home')
+    path('', views.teka_home, name='teka_home'),
+    path('publish', views.publish, name='publish')
 ]
