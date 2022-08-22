e5f83c217704df434e44cf6d8a397efa671eff3f
VladOs <cherednichenkov2000@gmail.com>
2022-08-22 16:55:46 +0300

    довипендрювався і нічого не работає

--- dzju/main/static/main/js/main.js
+++ dzju/main/static/main/js/main.js
@@ -1,4 +1,3 @@
-var geners = document.getElementById("geners");
 var gen =[["Бойовик", "bojovic"], ["Військовий", "vijskovij"], ["Детектив", "detectiv"],
             ["Дитячий", "detskij"], ["Домашній", "domashnij"], ["Драма", "drama"],
             ["Історичний", "istorikal"], ["Казка", "kazka"], ["Класика", "klasika"],
@@ -8,16 +7,5 @@ var gen =[["Бойовик", "bojovic"], ["Військовий", "vijskovij"], 
             ["Спорт", "sport"], ["Триллер", "triller"], ["Жахи", "jahi"], ["Містика", "mistik"],
             ["Фантастика", "fantastik"], ["Фентезі", "fentezi"]];
 //html = '<ul>';
-gen.forEach(function(item, i, gen) {
-//    alert( i + ": " + item[1] );
-    var br = document.createElement('br');
-    var a = document.createElement('a');
-    var linkText = document.createTextNode(item[0]);
-    a.appendChild(linkText);
-    a.title = item[0];
-    a.href = "http://127.0.0.1:8000/teka/"+item[1];
-    geners.appendChild(a);
-    geners.appendChild(br);
-//  html += '<li>'+item+'</li>';
-});
+
 //html += '</ul>'
--- dzju/teka/urls.py
+++ dzju/teka/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('catalog', views.teka, name='catalog'),
     path('publish', views.publish, name='publish'),
     path('crazy', views.crazy, name='crazy'),
-    path('crazy/upload', views.upload_book, name='upload_book'),
-    path('catalog/<int:pk>', views.CatalogDetailView.as_view(), name='catalog_detail')
+    path('crazy/download', views.download, name='download'),
+    path('catalog/<int:pk>', views.CatalogDetailView.as_view(), name='catalog_detail'),
+    path('catalog/<int:pk>', views.LoadFile.as_view(), name='load_file'),
+    # re_path(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
